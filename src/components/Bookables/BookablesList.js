fdab567ef412f3f1ba913535af8c67901ac59739
dabeat <dabeat1@hotmail.com>
2021-02-10 00:10:28 +0000

    bookablesVersion2withUseStateInstead

--- src/components/Bookables/BookablesList.js
+++ src/components/Bookables/BookablesList.js
@@ -1,5 +1,5 @@
-import React, { Fragment, useEffect, useRef } from "react"; //minus uyseState
-import { bookables, sessions, days } from "../../static.json";
+import React, { Fragment, useEffect, useRef, useState } from "react";
+//import { bookables, sessions, days } from "../../static.json";
 import { FaArrowRight } from "react-icons/fa";
 import Spinner from "../UI/Spinner";
 //import reducer from "./reducer";
@@ -15,15 +15,20 @@ import getData from "../../utils/api";
 //   error: false,
 // };
 
-export default function BookablesList({ state, dispatch }) {
-  const {
-    group,
-    bookableIndex,
-    bookables,
-    hasDetails,
-    isLoading,
-    error,
-  } = state;
+export default function BookablesList({ bookable, setBookable }) {
+  // const {
+  //   group,
+  //   bookableIndex,
+  //   bookables,
+  //   hasDetails,
+  //   isLoading,
+  //   error,
+  // } = state;
+  const [bookables, setBookables] = useState([]);
+  const [error, setError] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const group = bookable?.group;
 
   //const [group, setGroup] = useState("Kit");
   const bookablesInGroup = bookables.filter((b) => b.group === group);
@@ -31,28 +36,41 @@ export default function BookablesList({ state, dispatch }) {
   //const [bookableIndex, setBookableIndex] = useState(0);
   const groups = [...new Set(bookables.map((b) => b.group))];
 
-  const bookable = bookablesInGroup[bookableIndex]; // looks lime we not using
+  //const bookable = bookablesInGroup[bookableIndex]; // looks lime we not using
   //const [hasDetails, setHasDetails] = useState(false);
 
   //const timerRef = useRef(null);
   const nextButtonRef = useRef();
 
   useEffect(() => {
-    dispatch({ type: "FETCH_BOOKABLES_REQUEST" });
     getData("http://localhost:3001/bookables")
-      .then((bookables) =>
-        dispatch({
-          type: "FETCH_BOOKABLES_SUCCESS",
-          payload: bookables,
-        })
-      )
-      .catch((error) =>
-        dispatch({
-          type: "FETCH_BOOKABLES_ERROR",
-          payload: error,
-        })
-      );
-  }, [dispatch]);
+      .then((bookables) => {
+        setBookable(bookables[0]);
+        setBookables(bookables);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setIsLoading(false);
+      });
+  }, [setBookable]);
+
+  // useEffect(() => {
+  //   dispatch({ type: "FETCH_BOOKABLES_REQUEST" });
+  //   getData("http://localhost:3001/bookables")
+  //     .then((bookables) =>
+  //       dispatch({
+  //         type: "FETCH_BOOKABLES_SUCCESS",
+  //         payload: bookables,
+  //       })
+  //     )
+  //     .catch((error) =>
+  //       dispatch({
+  //         type: "FETCH_BOOKABLES_ERROR",
+  //         payload: error,
+  //       })
+  //     );
+  // }, [dispatch]);
 
   // useEffect(() => {
   //   timerRef.current = setInterval(() => {
@@ -66,10 +84,14 @@ export default function BookablesList({ state, dispatch }) {
   // }
 
   function changeGroup(event) {
-    dispatch({
-      type: "SET_GROUP",
-      payload: event.target.value,
-    });
+    const bookablesInSelectedGroup = bookables.filter(
+      (b) => b.group === event.target.value
+    );
+    setBookable(bookablesInSelectedGroup[0]);
+    // dispatch({
+    //   type: "SET_GROUP",
+    //   payload: event.target.value,
+    // });
     // setGroup(event.target.value);
     // setBookableIndex(0);
   }
@@ -77,15 +99,24 @@ export default function BookablesList({ state, dispatch }) {
   // function nextBookable() {
   //   setBookableIndex((i) => (i + 1) % bookablesInGroup.length);
   // }
-  function changeBookable(selectedIndex) {
-    dispatch({
-      type: "SET_BOOKABLE",
-      payload: selectedIndex,
-    });
+  function changeBookable(selectedBookable) {
+    setBookable(selectedBookable);
     nextButtonRef.current.focus();
   }
+  // function changeBookable(selectedIndex) {
+  //   dispatch({
+  //     type: "SET_BOOKABLE",
+  //     payload: selectedIndex,
+  //   });
+  //   nextButtonRef.current.focus();
+  // }
   function nextBookable() {
-    dispatch({ type: "NEXT_BOOKABLE" });
+    const i = bookablesInGroup.indexOf(bookable);
+    const nextIndex = (i + 1) % bookablesInGroup.length;
+    const nextBookable = bookablesInGroup[nextIndex];
+    setBookable(nextBookable);
+
+    //dispatch({ type: "NEXT_BOOKABLE" });
   }
   // function toggleDetails() {
   //   dispatch({ type: "TOGGLE_HAS_DETAILS" });
@@ -112,8 +143,8 @@ export default function BookablesList({ state, dispatch }) {
         </select>
         <ul className="bookables items-list-nav">
           {bookablesInGroup.map((b, i) => (
-            <li key={b.id} className={i === bookableIndex ? "selected" : null}>
-              <button className="btn" onClick={() => changeBookable(i)}>
+            <li key={b.id} className={b.id === bookable.id ? "selected" : null}>
+              <button className="btn" onClick={() => changeBookable(b)}>
                 {b.title}
               </button>
             </li>

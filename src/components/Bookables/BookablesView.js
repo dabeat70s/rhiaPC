bce844ac31204da408008f1f2ba98dc98217c9fc
dabeat <dabeat1@hotmail.com>
2021-02-11 04:46:16 +0000

    useCallback 4 funcs as props

--- src/components/Bookables/BookablesView.js
+++ src/components/Bookables/BookablesView.js
@@ -1,6 +1,6 @@
-import React, {useState, Fragment,useReducer} from 'react';
-import BookableDetails from './BookableDetails';
-import BookablesList from './BookablesList';
+import React, { useState, Fragment, useCallback } from "react";
+import BookableDetails from "./BookableDetails";
+import BookablesList from "./BookablesList";
 // import reducer from './reducer';
 
 // const initialState = {
@@ -11,19 +11,35 @@ import BookablesList from './BookablesList';
 //     error: false
 // };
 
-export default function BookablesView ()  {
-    const [bookable, setBookable] = useState();
-    // const [state,dispatch] = useReducer(reducer,initialState);
+export default function BookablesView() {
+  const [bookable, setBookable] = useState();
 
-    // const bookablesInGroup = state.bookables.filter(
-    //     b=> b.group === state.group
-    // );
-    // const bookable = bookablesInGroup[state.bookableIndex]
-    return (
-        <Fragment>
-            {/* <BookablesList state={state} dispatch={dispatch} /> */}
-            <BookablesList bookable={bookable} setBookable={setBookable}/>
-            <BookableDetails bookable={bookable} />
-        </Fragment>
-    )
+  // causing rerenders etc.. infinite loops
+  // function updateBookable (selected) {
+  //     if (selected) {
+  //     selected.lastShown = Date.now();
+  //     setBookable(selected);
+  //     }
+  // }
+  const updateBookable = useCallback((selected) => {
+    if (selected) {
+      selected.lastShown = Date.now();
+      setBookable(selected);
+    }
+  }, []);
+
+  // const [state,dispatch] = useReducer(reducer,initialState);
+
+  // const bookablesInGroup = state.bookables.filter(
+  //     b=> b.group === state.group
+  // );
+  // const bookable = bookablesInGroup[state.bookableIndex]
+  return (
+    <Fragment>
+      {/* <BookablesList state={state} dispatch={dispatch} /> */}
+      {/* <BookablesList bookable={bookable} setBookable={setBookable}/> */}
+      <BookablesList bookable={bookable} setBookable={updateBookable} />
+      <BookableDetails bookable={bookable} />
+    </Fragment>
+  );
 }
